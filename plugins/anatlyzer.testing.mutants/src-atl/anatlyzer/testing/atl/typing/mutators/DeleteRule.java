273f91228f322680f1fd7990804fe3e14006c92e
Esther Guerra <esther.guerra@uam.es>
2019-04-21 12:13:32 +0200

    DeleteRule mutation operator

---
+++ plugins/anatlyzer.testing.mutants/src-atl/anatlyzer/testing/atl/typing/mutators/DeleteRule.java
@@ -0,0 +1,10 @@
+package anatlyzer.testing.atl.typing.mutators;
+
+import anatlyzer.testing.atl.mutators.deletion.RuleDeletionMutator;
+
+public class DeleteRule extends RuleDeletionMutator {
+	@Override
+	public String getDescription() {
+		return "DeleteRule";
+	}	
+}
